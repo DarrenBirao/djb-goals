32f1f6fb805b62164f6795c28e95ae2c3b5846f9
DarrenBirao <djbirao01@gmail.com>
2025-08-27 15:34:27 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAQdnF4LDqgfOkR7KngoYB0FfLoiuSQJ4",
+  authDomain: "djb-goals.firebaseapp.com",
+  projectId: "djb-goals",
+  storageBucket: "djb-goals.firebasestorage.app",
+  messagingSenderId: "421566972547",
+  appId: "1:421566972547:web:bcb445dc73bc69cf4b91b6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
